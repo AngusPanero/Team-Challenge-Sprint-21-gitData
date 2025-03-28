8a283d5d82ab6b294c29388b07dc4bc84b44c631
Agustin Panero <agustinpanero@MacBook-Pro-de-Agustin.local>
2025-03-28 17:14:53 +0100

    TERMINADO

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,92 @@
-function App() {
-  
+import { useState, useEffect } from "react";
+import { addUser, removeUser } from "./userSlice"
+import { useSelector, useDispatch } from "react-redux";
+import "./App.css"
+
+const App = () => {
+  const dispatch = useDispatch()
+  const users = useSelector((state) => state.userSelector.users)
+
+  const [ dataApi, setDataApi ] = useState(null)
+  const [ error, setError ] = useState("")
+  const [ loading, setLoading ] = useState(false)
+  const [ userInput, setUserInput ] = useState("")
+
+  const getUsers = async () => {
+    if(!userInput) return
+    try {
+      setLoading(true)
+      const response = await fetch(`https://api.github.com/users/${userInput}`)
+        
+      const data = await response.json()
+      console.log(data);
+      
+      setDataApi(data)
+      setError("")
+
+    } catch (error) {
+        setError(error.message)
+    } finally{
+      setLoading(false)
+    }
+  }
+
+  const handleRemoveUser = (id) => {
+    dispatch(removeUser(id));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getUsers();
+  };
+
+  useEffect(() => {
+    if (dataApi) {
+      const existingUser = users.find((user) => user.id === dataApi.id);
+      if (!existingUser) {
+        dispatch(
+          addUser({
+            id: dataApi.id,
+            username: dataApi.login,
+            avatar: dataApi.avatar_url,
+            followers: dataApi.followers,
+            following: dataApi.following,
+          })
+        );
+      }
+      setDataApi(null);
+    }
+  }, [dataApi, users, dispatch]);
+
+
+  if(error) return <h1>Error in Request</h1>
+  if(loading) return <h1>Loading GitHub User</h1>
 
   return (
-  <></>
+    <>
+      <div className="searcher">
+        <h1>Search Your GitHub Profile</h1> 
+        <div className="h1-input">
+          <input className="user-input" type="text" value={userInput} onChange={(e) => setUserInput(e.target.value)} placeholder="Introduce an User"/> 
+          <button className="user-search" type="button" onClick={handleSubmit}>Search</button>
+        </div>
+      </div>
+
+      {users.length > 0 && (
+          <div className="users">
+            {users.map((user) => (
+              <div className="username-card" key={user.id}>
+                <img src={user.avatar}className="photo"alt="profile-photo"/>
+                <p>{user.username}</p>
+                <p>ID: {user.id}</p>
+                <p>Followers: {user.followers}</p>
+                <p>Following: {user.following}</p>
+                <button onClick={() => handleRemoveUser(user.id)}>Eliminar</button>
+              </div>
+            ))}
+          </div>
+        )}
+    </>
   );
 }
 
